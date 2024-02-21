a0eebb2aaf1e1d8620498c7c5afbdec12bf51534
Alvaro <alvarorent2001@gmail.com>
2024-02-21 09:48:35 -0500

    update

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 // permitir solicitudes desde cualquier origen
 app.use(
   cors({
-    origin: "https://starwars-tasks.netlify.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials:true
