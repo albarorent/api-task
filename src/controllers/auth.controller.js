b35f36553f2e8922f1cd8fc7ee7fe4c0ff341375
Alvaro <alvarorent2001@gmail.com>
2024-02-21 11:43:46 -0500

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -53,8 +53,6 @@ export const login = async (req, res) => {
     const token = await createAccessToken({ id: userFound._id });
 
     res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // Ejemplo: la cookie expirará en 7 días
     });
