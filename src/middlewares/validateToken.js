7148309534aec6586da4539925cb17b29a2fe5e5
Alvaro <alvarorent2001@gmail.com>
2024-02-21 10:01:25 -0500

    update

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,9 +2,7 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 
 export const authRequired = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "React app URL");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+ 
   const { token } = req.cookies;
   // console.log("valores:", req.cookies);
   console.log(req.cookies);
