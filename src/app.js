2dedeeedc5b483ffa6f39ed687b8163c3c954db9
Alvaro <alvarorent2001@gmail.com>
2024-02-21 10:10:03 -0500

    update

--- src/app.js
+++ src/app.js
@@ -11,15 +11,12 @@ const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type",
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
