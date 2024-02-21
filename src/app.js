3f226438a323d7381512fd428106be56a444dbfc
Alvaro <alvarorent2001@gmail.com>
2024-02-21 11:43:12 -0500

    update

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(
   cors({
     origin: ['http://localhost:5173', 'https://starwars-tasks.netlify.app'],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials:true
   })
 );
