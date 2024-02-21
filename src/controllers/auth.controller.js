78e1781ad44d2c1591914b2bd101abd9961846bb
Alvaro <alvarorent2001@gmail.com>
2024-02-21 10:38:50 -0500

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -52,6 +52,10 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000, // Ejemplo: la cookie expirará en 7 días
+      domain: 'localhost',
+      path: '/',
+      secure: false, // Para desarrollo en localhost no se necesita HTTPS
+      httpOnly: true
     });
     // console.log("token login:", token);
     res.json({
