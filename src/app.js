802c64cb9a96648032a7b694fea484ce294d6042
Alvaro <alvarorent2001@gmail.com>
2024-02-21 10:01:05 -0500

    update

--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
-
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+    credentials:true
   })
 );
-
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
