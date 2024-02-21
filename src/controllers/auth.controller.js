3dab4ca5d890cc6be2fc9e344010415bb38c043b
Alvaro <alvarorent2001@gmail.com>
2024-02-20 20:37:39 -0500

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -47,7 +47,7 @@ export const login = async (req, res) => {
 
     const token = await createAccessToken({ id: userFound._id });
 
-    res.cookie("token", token, { domain: "api-tasks-u571.onrender.com", secure: true, sameSite: "None" });
+    res.cookie("token", token);
     console.log('token login:',token);
     res.json({
       id: userFound._id,
