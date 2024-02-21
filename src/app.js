93255ceee5619ce5f6c2f25668c4803e94586b8d
Alvaro <alvarorent2001@gmail.com>
2024-02-21 09:52:06 -0500

    update

--- src/app.js
+++ src/app.js
@@ -8,12 +8,18 @@ import taskRoutes from "./routes/tasks.routes.js";
 const app = express();
 
 // permitir solicitudes desde cualquier origen
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "React app URL");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-    credentials:true
+
+    credentials: true,
   })
 );
 app.use(morgan("dev"));
